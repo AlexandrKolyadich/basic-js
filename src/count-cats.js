33a4f237ecbdca7c18ad50f5fea6f9d223c8f737
AlexandrKolyadich <sanesanasa@gmail.com>
2020-02-20 14:30:43 +0300

    cat compl

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,8 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+	var number = 0;
+  backyard.forEach(function(item, i,backyard){
+  	if(item === "^^")
+  		number++;
+  });
+  return number;
 };
